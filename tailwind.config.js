25175d52bc18676c351e654ba9e147ea8ae2110c
Rizal <fahrizal.maruf@gmail.com>
2022-03-24 15:24:51 +0800

    move animation style from tailwind extend into its own file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,41 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        rubber: {
+          "from, to": {
+            transform: "scale3d(1, 1, 1)",
+          },
+
+          "30%": {
+            transform: "scale3d(1.25, 0.75, 1)",
+          },
+
+          "40%": {
+            transform: "scale3d(0.75, 1.25, 1)",
+          },
+
+          "50%": {
+            transform: "scale3d(1.15, 0.85, 1)",
+          },
+
+          "65%": {
+            transform: "scale3d(.95, 1.05, 1)",
+          },
+
+          "75%": {
+            transform: "scale3d(1.05, .95, 1)",
+          },
+        },
+      },
+      animation: {
+        rubber: "rubber 0.5s both",
+      },
+    },
   },
   plugins: [],
 };
