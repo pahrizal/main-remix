06cebcd6ece5fe4eeda4df26013e9f2a5beb801e
Rizal <fahrizal.maruf@gmail.com>
2022-03-24 19:01:53 +0800

    update build script

--- app/stores/config.ts
+++ app/stores/config.ts
@@ -5,8 +5,8 @@ import { AppState, reducers } from "./index";
 const bindMiddleware = (middleware: any) => {
   if (process.env.NODE_ENV !== "production") {
     // I require this only in dev environment
-    const { composeWithDevTools } = require("redux-devtools-extension");
-    return composeWithDevTools(applyMiddleware(...middleware));
+    // const { composeWithDevTools } = require("redux-devtools-extension");
+    // return composeWithDevTools(applyMiddleware(...middleware));
   }
   return applyMiddleware(...middleware);
 };
