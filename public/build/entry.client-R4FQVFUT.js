88e31699a96cf6d45eb0f68e8620379d467baeeb
Rizal <fahrizal.maruf@gmail.com>
2022-03-24 19:41:34 +0800

    add heroku postbuild

--- public/build/entry.client-R4FQVFUT.js
+++
@@ -1,1 +0,0 @@
-import{a as t,h as o}from"/build/_shared/chunk-JXKH42LX.js";import{d as i,g as r,h as m}from"/build/_shared/chunk-SPSU65KA.js";m();var e=i(t());(0,e.hydrate)(r.createElement(o,null),document);
