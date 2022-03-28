3994a79539c11edd21702e8af7c7fb1885b8fab2
PAHRIZAL MA'RUP <36581242+pahrizal@users.noreply.github.com>
2022-03-28 16:01:18 +0800

    fix import in index file

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "remix";
 import { v4 as uuidv4 } from "uuid";
 import Button from "~/components/Button";
-import playingCardBackground from "../assets/images/playing-cards.png";
+import playingCardBackground from "~/assets/images/playing-cards.png";
 import RubberText from "~/components/RubberText";
 import { AppState } from "~/stores";
 import { gameActions } from "~/stores/gameState";
