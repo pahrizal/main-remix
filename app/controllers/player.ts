e0043def9f7366b4752554b11068ab84f7eb0269
Rizal <fahrizal.maruf@gmail.com>
2022-03-31 19:32:45 +0800

    add one bot when only the onwer of the game start playing

--- app/controllers/player.ts
+++ app/controllers/player.ts
@@ -2,11 +2,12 @@ import { printLog, randomColor } from "~/utils/helper";
 import { Card } from "~/controllers/game";
 
 export interface PlayerData {
-  id: string;
-  name: string;
-  socketId: string;
-  cards?: Card[];
-  colors?: string;
+    id: string;
+    name: string;
+    socketId: string;
+    cards?: Card[];
+    colors?: string;
+    bot: boolean;
 }
 /**
  * This class is used to manage player in the game
@@ -14,47 +15,52 @@ export interface PlayerData {
  * @param {PlayerData} data - The player data
  */
 export default class PlayerController {
-  private data: PlayerData;
-  constructor(data: PlayerData) {
-    this.data = data;
-    this.data.colors = randomColor();
-  }
-  getSocketId() {
-    return this.data.socketId;
-  }
-  getName() {
-    return this.data.name;
-  }
-  setName(name: string) {
-    this.data.name = name;
-  }
-  setSocketId(socketId: string) {
-    this.data.socketId = socketId;
-  }
-  getId() {
-    return this.data.id;
-  }
-  getData(opts?: { includeCards?: boolean }) {
-    return opts?.includeCards ? this.data : { ...this.data, cards: [] };
-  }
-  setCards(cards: Card[]) {
-    this.data.cards = cards;
-  }
-  removeCard(card: Card) {
-    if (!this.data.cards) return;
-    printLog("info", "PLAYER", "Removing card: ", card);
-    const newCards = this.data.cards.filter((c) => c.code !== card.code);
-    this.data.cards = newCards;
-  }
-  getCards() {
-    return this.data.cards || [];
-  }
-  leave() {
-    this.data = {
-      id: "",
-      name: "",
-      socketId: "",
-      cards: [],
-    };
-  }
+    private data: PlayerData;
+    constructor(data: PlayerData) {
+        this.data = data;
+        this.data.colors = randomColor();
+    }
+    getSocketId() {
+        return this.data.socketId;
+    }
+    getName() {
+        return this.data.name;
+    }
+    setName(name: string) {
+        this.data.name = name;
+    }
+    setSocketId(socketId: string) {
+        this.data.socketId = socketId;
+    }
+    getId() {
+        return this.data.id;
+    }
+    getData(opts?: { includeCards?: boolean }) {
+        return opts?.includeCards ? this.data : { ...this.data, cards: [] };
+    }
+    setCards(cards: Card[]) {
+        this.data.cards = cards;
+    }
+    removeCard(card: Card) {
+        if (!this.data.cards) return;
+        printLog("info", "PLAYER", "Removing card: ", card);
+        const newCards = this.data.cards.filter((c) => c.code !== card.code);
+        this.data.cards = newCards;
+    }
+    getCards() {
+        return this.data.cards || [];
+    }
+    isBot() {
+        return this.data.bot;
+    }
+
+    leave() {
+        this.data = {
+            id: "",
+            name: "",
+            socketId: "",
+            cards: [],
+            bot: false,
+        };
+    }
 }
