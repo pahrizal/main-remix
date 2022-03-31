b944ce8b6626059b0dd3e4f38d9fd5a1598b5100
Rizal <fahrizal.maruf@gmail.com>
2022-03-31 22:36:12 +0800

    remove unused component

--- app/routes/test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import GameResult from "~/components/GameResult";
-
-const Test = () => {
-    return <GameResult isTheWinner={true} id={""} name={""} socketId={""} bot={false} />;
-};
-
-export default Test;
